a31e15129abac25acae783876f15469ad66d314d
Christoph Blattgerste <blattgerste@stud.uni-heidelberg.de>
2024-09-28 12:10:19 +0200

    merge conflict

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
 
     args_list = [(i, False) for i in range(200000, 500000)]
 
-    with multiprocessing.Pool(processes=28) as p:
+    with multiprocessing.Pool(processes=12) as p:
         results = list(tqdm(p.imap(parallel_process_pdf, args_list), total=len(args_list)))
     
     print("Process completed.")
