c1af5af41ce398997c634de8f1ba26455597c4c4
Christoph Blattgerste <blattgerste@stud.uni-heidelberg.de>
2024-09-29 22:45:02 +0200

    added folder name to nextcloud functions

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -12,11 +12,11 @@ import extractor
 nextcloud_folder = "CouncilDocuments"
 nextcloud_user = "TeamAgenda"
 nextcloud_password = "TeamAgenda@2024"
-nextcloud_url = f"https://nc-1578619932403564675.nextcloud-ionos.com/remote.php/dav/files/{nextcloud_user}/{nextcloud_folder}/"
+nextcloud_url = f"https://nc-1578619932403564675.nextcloud-ionos.com/remote.php/dav/files/{nextcloud_user}"
 
 
-def download_from_nextcloud(filename):
-    nextcloud_file_path = nextcloud_url + filename
+def download_from_nextcloud(folder, filename):
+    nextcloud_file_path = os.path.join(nextcloud_url, folder, filename)
     response = requests.get(
         nextcloud_file_path,
         auth=HTTPBasicAuth(nextcloud_user, nextcloud_password)
@@ -28,13 +28,13 @@ def download_from_nextcloud(filename):
         return None
 	
 
-def upload_to_nextcloud(filename, content, content_type="binary", verbose=False):
+def upload_to_nextcloud(folder, filename, content, content_type="binary", verbose=False):
 	"""
 	Upload the given content (PDF or text data) to Nextcloud.
 	If content_type is 'text', it will upload the content as text (encoded in utf-8),
 	otherwise it will treat it as binary content.
 	"""
-	nextcloud_file_path = nextcloud_url + filename
+	nextcloud_file_path = os.path.join(nextcloud_url, folder, filename)
 
 	# Encode text if content type is 'text'
 	if content_type == "text":
@@ -60,19 +60,19 @@ def process_pdf(idx, verbose=False):
 	"""
 	Process the PDF by downloading, uploading to Nextcloud, extracting text, and uploading the text file to Nextcloud.
 	"""
-	
+
 	pdf_content = download.request_pdf(idx)  # Request the PDF file
 
 	if pdf_content:
 		filename = f"{idx}.pdf"
-		upload_successful = upload_to_nextcloud(filename, pdf_content, content_type="binary")  # Upload the PDF to Nextcloud
+		upload_successful = upload_to_nextcloud(nextcloud_folder, filename, pdf_content, content_type="binary")  # Upload the PDF to Nextcloud
 
 		if upload_successful:
 			doc = pymupdf.open(stream=pdf_content, filetype="pdf")  # Extract text from the downloaded PDF
 			text = extractor.extract_text(doc)
 
 			text_filename = f"{idx}.txt"  # Save the extracted text to a local file
-			upload_to_nextcloud(text_filename, text, content_type="text")
+			upload_to_nextcloud(nextcloud_folder, text_filename, text, content_type="text")
 
 			if verbose:
 				print(f"Text extracted and saved for {filename} as {text_filename}")
