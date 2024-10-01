a6d38e8e25d3c5b04937c25fa891223573b82680
Christoph Blattgerste <blattgerste@stud.uni-heidelberg.de>
2024-10-01 09:08:16 +0200

    using HuggingFace LLM and Embedding model with FAISS vector store

--- llm_rag/pipeline.py
+++ llm_rag/pipeline.py
@@ -1,14 +1,16 @@
 import pickle
 import torch
 import faiss
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.docstore import InMemoryDocstore
-from langchain_community.vectorstores import FAISS
-from llama_index.core import Settings
-from llama_index.core import VectorStoreIndex, StorageContext
-from llama_index.core.storage.index_store import SimpleIndexStore
+from llama_index.core import Settings, load_index_from_storage
+from llama_index.core import ServiceContext, StorageContext
+from llama_index.vector_stores.faiss import FaissVectorStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from huggingface_hub import login
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+import torch
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core import PromptTemplate
 
 
 def load_llama_model(model_name, token):
@@ -17,35 +19,11 @@ def load_llama_model(model_name, token):
     model.eval()
     return tokenizer, model
 
-
-def load_faiss_index(faiss_index_path="faiss_index", embedding_model_name="sentence-transformers/all-MiniLM-L6-v2"):
-    # Load FAISS store from the saved directory with the embedding model used for indexing
-    embedding_model = HuggingFaceEmbeddings(model_name=embedding_model_name)
-
-    faiss_store = FAISS.load_local(faiss_index_path, embeddings=embedding_model, allow_dangerous_deserialization=True)
-    print(f"FAISS store loaded from {faiss_index_path}.")
-    return faiss_store
-
-
-def load_llama_index(faiss_store):
-    docstore = InMemoryDocstore({})
-    index_store = SimpleIndexStore()
-    vector_stores = {"default_namespace": faiss_store}  # The FAISS store with a namespace
-    
-    # Initialize StorageContext with docstore, index_store, and vector_stores
-    storage_context = StorageContext(
-        docstore=docstore,
-        index_store=index_store,
-        vector_stores=vector_stores,
-        graph_store=None  # Optional
-    )
-    print("StorageContext initialized.")
-
-    Settings.embed_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    llama_index = VectorStoreIndex([], storage_context=storage_context)
-    print("LlamaIndex initialized.")
-    
-    return llama_index
+def initialize_embedding_model():
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    embedding_model = HuggingFaceEmbedding(model_name=model_name)
+    print(f"Embedding model {model_name} initialized.")
+    return embedding_model
 
     
 def huggingface_login():
@@ -74,15 +52,52 @@ def query_rag_system(query, llama_index, model, tokenizer):
 
 
 if __name__ == "__main__":
-    token = "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"
+    # token = "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"
+    huggingface_login()
     model_name = "meta-llama/Llama-3.1-8B"
-    # tokenizer, model = load_llama_model(model_name, token)
-    
-    faiss_path = "../preprocessing/faiss_index"
-    faiss_store = load_faiss_index(faiss_index_path=faiss_path)
-    llama_index = load_llama_index(faiss_store)
+    embed_model = initialize_embedding_model()
+
+    SYSTEM_PROMPT = """You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
+    - Generate human readable output, avoid creating output with gibberish text.
+    - Generate only the requested output, don't include any other language before or after the requested output.
+    - Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
+    - Generate professional language typically used in business documents in North America.
+    - Never generate offensive or foul language.
+    """
+
+    query_wrapper_prompt = PromptTemplate(
+        "[INST]<>\n" + SYSTEM_PROMPT + "<>\n\n{query_str}[/INST] "
+    )
+
+    llm_model = HuggingFaceLLM(
+        context_window=4096,
+        max_new_tokens=2048,
+        generate_kwargs={"temperature": 0.0, "do_sample": False},
+        query_wrapper_prompt=query_wrapper_prompt,
+        tokenizer_name=model_name,
+        model_name=model_name,
+        device_map="auto",
+        # change these settings below depending on your GPU
+        # model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": True},
+    )
+
+    Settings.llm = llm_model
+    Settings.embed_model = embed_model
+    # faiss_path = "faiss_vectorstore_index.json"
+    # faiss_store = load_faiss_index(faiss_index_path=faiss_path)
+
+    # vector_store = FaissVectorStore.from_persist_dir("../preprocessing/vectorstore_index")
+    storage_context = StorageContext.from_defaults(persist_dir="../preprocessing/vectorstore_index")
+    # service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=llm_model)
+    index = load_index_from_storage(storage_context)
 
     # Example query
+    # query = "What was approved in the council meeting?"
+    # answer = query_rag_system(query, llama_index, model, tokenizer)
+    # print(f"Answer: {answer}")
+
+    # set Logging to DEBUG for more detailed outputs
+    query_engine = index.as_query_engine()
     query = "What was approved in the council meeting?"
-    answer = query_rag_system(query, llama_index, model, tokenizer)
-    print(f"Answer: {answer}")
+    response = query_engine.query(query)
+    print(response)
