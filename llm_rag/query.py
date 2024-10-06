154c81f9bbe6e0c2269447240a0dfd6db8648e87
Christoph Blattgerste <christoph.blattgerste@med.uni-heidelberg.de>
2024-10-06 13:03:45 +0000

    query engine and personalized prompt

--- llm_rag/query.py
+++ llm_rag/query.py
@@ -2,13 +2,16 @@ import os
 import torch
 import faiss
 from llama_index.core import Settings, load_index_from_storage
-from llama_index.core import ServiceContext, StorageContext
+from llama_index.core import StorageContext
 from llama_index.core import PromptTemplate
+from llama_index.core import VectorStoreIndex, get_response_synthesizer
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.huggingface import HuggingFaceLLM
 from huggingface_hub import login
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer
 
 
 def huggingface_login():
@@ -27,23 +30,31 @@ def init_llm_model(llm_name, token):
         tokenizer.convert_tokens_to_ids("<|eot_id|>"),
     ]
 
+    system_prompt = """Du bist ein intelligentes System, das deutsche Dokumente durchsucht und auf Basis der enthaltenen Informationen präzise Antworten auf gestellte Fragen gibt. Wenn du eine Antwort formulierst, gib die Antwort in klaren und präzisen Sätzen an und nenne dabei mindestens eine oder mehrere relevante Quellen im Format: (Quelle: Dokumentname, Abschnitt/Seite, Filename des TXT)."""
+    # This will wrap the default prompts that are internal to llama-index
+    query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
+
     model = HuggingFaceLLM(
+        context_window=4096,
+        max_new_tokens=1024,
         model_name=llm_name,
         model_kwargs={
             "token": token,
             "torch_dtype": torch.bfloat16,  # comment this line and uncomment below to use 4bit
+            # "load_in_8bit": True,
             # "quantization_config": quantization_config
         },
         device_map="cuda",
         generate_kwargs={
-            "do_sample": True,
+            "do_sample": True, 
             "temperature": 0.6,
             "top_p": 0.9,
         },
+        system_prompt=system_prompt,
+        query_wrapper_prompt=query_wrapper_prompt,
         tokenizer_name=llm_name,
         tokenizer_kwargs={"token": token},
-
-        stopping_ids=stopping_ids,
+        # stopping_ids=stopping_ids,
     )
 
     return tokenizer, model
@@ -54,55 +65,79 @@ def init_embedding_model(embed_name):
     print(f"Embedding model {embed_name} initialized.")
     return embedding_model
 
-    
-def query_response(query, index):
-    query_engine = index.as_query_engine()
-    response = query_engine.query(query)
 
-    documents = [doc.text for doc in response.source_nodes]
-    context = "\n".join(documents)
-    
-    tokenizer = Settings.tokenizer
-    model = Settings.llm
-    
-    input_text = f"Frage:\n{query}\n\nKontext: {query}\nAntwort:"
-    inputs = tokenizer(input_text, return_tensors="pt")
-    with torch.no_grad():  # No gradients needed for inference
-        outputs = model.generate(**inputs, max_new_tokens=150)
-    
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return response
+def load_index_storage(index_dir):
+
+    faiss_index = faiss.read_index(os.path.join(index_dir, "faiss_index.idx"))
+    faiss_store = FaissVectorStore(faiss_index=faiss_index)
+    storage_context = StorageContext.from_defaults(vector_store=faiss_store, persist_dir=index_dir)
+    index = load_index_from_storage(storage_context)
+    print(f"Number of vectors stored: {faiss_index.ntotal}")
+    print(f"Number of nodes in index: {len(index.ref_doc_info)}")
+
+    return index
+
+
+def configure_query_engine(index) -> RetrieverQueryEngine:
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=2,
+    )
+
+    response_synthesizer = get_response_synthesizer(
+        response_mode="tree_summarize",
+    )
+
+    query_engine = RetrieverQueryEngine(
+        retriever=retriever,
+        response_synthesizer=response_synthesizer,
+    )
+
+    summary_prompt =  (
+        "Nachfolgend sind passensten Kontextinformationen.\n"
+        "---------------------\n"
+        "{context_str}\n"
+        "---------------------\n"
+        "Du bist ein intelligentes System, das diese deutschen Kontextinformationen durchsucht und auf Basis der enthaltenen Informationen präzise Antworten auf gestellte Fragen gibt. Wenn du eine Antwort in klaren und präzisen Sätzen formulierst, nenne dabei mindestens eine oder mehrere relevante Quellen auf die entsprechenden Textstellen des Kontexts im Format: (Quelle: Dokumentname, Abschnitt/Seite).\n"
+        "Query: {query_str}\n"
+        "Antwort: "
+    )
+    prompt_template = PromptTemplate(summary_prompt)
+    query_engine.update_prompts(
+        {"response_synthesizer:summary_template": prompt_template}
+    )
+
+    return query_engine
+
+
+def display_prompt_dict(prompts_dict):
+    for k, p in prompts_dict.items():
+        text_md = f"**Prompt Key**: {k}<br>" f"**Text:** <br>"
+        print(text_md)
+        print(p.get_template())
 
 
 if __name__ == "__main__":
 
     token = "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"
     huggingface_login()
-    embed_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    embed_name = "dunzhang/stella_en_1.5B_v5"
     llm_name = "meta-llama/Meta-Llama-3.1-8B"
-    index_dir = "../CouncilEmbeddings/vectorstore_index_chunked"
-    # llm_name = "Intel/dynamic_tinybert"
+    # index_dir = "../CouncilEmbeddings/vectorstore_index_chunked"
+    index_dir = "../preprocessing/vectorstore_index"
     embed_model = init_embedding_model(embed_name)
 
     tokenizer, llm_model = init_llm_model(llm_name=llm_name, token=token)
 
     Settings.llm = llm_model
-    Settings.tokenizer = tokenizer
+    # Settings.tokenizer = tokenizer
     Settings.embed_model = embed_model
 
-    faiss_index = faiss.read_index(os.path.join(index_dir, "faiss_index.idx"))
-    faiss_store = FaissVectorStore(faiss_index=faiss_index)
-    storage_context = StorageContext.from_defaults(vector_store=faiss_store, persist_dir=index_dir)
-    index = load_index_from_storage(storage_context)
+    index = load_index_storage(index_dir)
+    query_engine = configure_query_engine(index)
+    # display_prompt_dict(prompts_dict)
 
-    print(f"Number of vectors stored: {faiss_index.ntotal}")
-    print(f"Number of nodes in index: {len(index.ref_doc_info)}")
-
-    # set Logging to DEBUG for more detailed outputs
-
-    query = "Welche Themen wurden in der letzten Sitzung des Gemeinderats besprochen?"
-    # response = query_response(query, index)
-    query_engine = index.as_query_engine()
+    query = "Welche Themen wurden in der letzten Sitzung des Gemeinderates besprochen?"
     response = query_engine.query(query)
 
 
